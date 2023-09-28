5b11c7c200eb705995569b73b3d615a628173445
Colin Beltz <colinbeltz@gmail.com>
2023-09-27 21:36:53 -0500

    Refactored code to move JobDetails into its own component.

--- src/common.js
+++ src/common.js
@@ -1,4 +1,9 @@
-// --- GLOBAL ---
+// CONSTANTS
+
+export const BASE_API_URL = 'https://bytegrad.com/course-assets/js/2/api';
+export const DEFAULT_DISPLAY_TIME = 3500;
+
+// SELECTORS
 export const bookmarksBtnEl = document.querySelector('.bookmarks-btn');
 
 export const errorTextEl = document.querySelector('.error__text');
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,14 @@
 import { 
+    BASE_API_URL,
     searchInputEl,
     searchFormEl,
     jobListSearchEl,
-    numberEl,
+    numberEl
 } from '../common.js';
 
 import renderError from './Error.js';
 import renderSpinner from './Spinner.js';
+import renderJobList from './JobList.js';
 
 
 // --- SEARCH COMPONENT ----
@@ -34,7 +36,7 @@ const submitHandler = event => {
         renderSpinner('search');
 
         // fetch search results
-        fetch(`https://bytegrad.com/course-assets/js/2/api/jobs?search=${searchText}`)
+        fetch(`${BASE_API_URL}/jobs?search=${searchText}`)
             .then(response => {
                 if (!response.ok) {
                     console.log('Something went wrong');
@@ -50,30 +52,7 @@ const submitHandler = event => {
 
                 // render number of results
                 numberEl.textContent = jobItems.length;
-
-                jobItems.slice(0, 7).forEach(jobItem => {
-                    const newJobItemHTML = `                
-                    <li class="job-item">
-                        <a class="job-item__link" href="${jobItem.id}">
-                            <div class="job-item__badge">${jobItem.badgeLetters}</div>
-                            <div class="job-item__middle">
-                                <h3 class="third-heading">${jobItem.title}</h3>
-                                <p class="job-item__company">${jobItem.company}</p>
-                                <div class="job-item__extras">
-                                    <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${jobItem.salary}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobItem.location}</p>
-                                </div>
-                            </div>
-                            <div class="job-item__right">
-                                <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                                <time class="job-item__time">${jobItem.daysAgo}d</time>
-                            </div>
-                        </a>
-                    </li>
-                `;
-                jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
-                });
+                renderJobList(jobItems);
             })
             .catch(error => console.log(error));
 };
